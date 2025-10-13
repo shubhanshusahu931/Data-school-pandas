2e17f3b0e6d0b2b57bf5dd0bbbbf0d2014e85a82
labhanshusahu268 <sahulabh268@gmail.com>
2025-10-14 01:35:07 +0530

    day 3

--- d03.py
+++ d03.py
@@ -1,1 +1,9 @@
 # This is d03.py
+import pandas as pd
+movies = pd.read_csv('https://raw.githubusercontent.com/justmarkham/pandas-videos/master/data/imdb_1000.csv')
+print(movies.head())
+print(movies.describe())
+print(movies.shape)
+print(movies.dtypes)
+print(type(movies))
+print(movies.describe(include=['object']))
